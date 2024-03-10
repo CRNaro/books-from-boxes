3ff6ff689d8272f2acde891d5a25fc96449ec9e7
CRNaro <144285637+CRNaro@users.noreply.github.com>
2024-03-10 15:42:02 -0400

    Build/Fix: Updated middleware in auth.js to w/ GraphQL and implimented Apollo Server in server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+// CRN: import ApolloServer
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
+// CRN: import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const routes = require('./routes');
+
+// CRN: create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req  })
+});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// CRN: integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
