aee830acaa65e8b6ef8f824a6883ec88cc197e71
CRNaro <144285637+CRNaro@users.noreply.github.com>
2024-03-20 17:04:40 -0400

    Build: trying to make it so my MongoDB Compass db will connect with my MongoDB Atlas so I can deploy on Render. This is infuritating

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI);
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://crnaro:<password>@cluster0.wpnvsne.mongodb.net/googlebooks');
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
