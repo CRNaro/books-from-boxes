f1be99527f971076ecffd5578f398a4ccc8d34dc
CRNaro <144285637+CRNaro@users.noreply.github.com>
2024-03-20 17:58:46 -0400

    Fix: still working on deploying to Render

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,3 +7,4 @@ mongoose.connect(process.env.MONGODB_URI);
 //mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
+//adding to save
